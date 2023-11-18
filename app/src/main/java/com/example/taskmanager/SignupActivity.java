8b83d775c71dd19c9c1b37b1d000d4df146eca09
Sunny Lam <77874599+sunnylam9876@users.noreply.github.com>
2023-11-17 23:26:55 -0800

    revised signup page
    add the function of adding user information to Firebase database

--- app/src/main/java/com/example/taskmanager/SignupActivity.java
+++ app/src/main/java/com/example/taskmanager/SignupActivity.java
@@ -12,9 +12,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -22,8 +33,14 @@ public class SignupActivity extends AppCompatActivity {
     private Button btnSignup;
     private TextView tvLoginMsg;
 
-    // for Firebase signup
+    // for Firebase Authentication
     private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    //Firebase connection
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +55,14 @@ public class SignupActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
+
         // sign up function
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String userEmail = etSignupEmail.getText().toString().trim();
                 String pass = etSignupPassword.getText().toString().trim();
+                String userName = etSignupName.getText().toString().trim();
 
                 if (userEmail.isEmpty()) {
                     etSignupEmail.setError("Email cannot be empty");
@@ -52,13 +71,51 @@ public class SignupActivity extends AppCompatActivity {
                 if (pass.isEmpty()) {
                     etSignupPassword.setError("Password cannot be empty");
                 } else {
+                    // create an user account
                     auth.createUserWithEmailAndPassword(userEmail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Toast.makeText(SignupActivity.this, "SignUp Successful", Toast.LENGTH_LONG).show();
-                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                                currentUser = auth.getCurrentUser();
+                                assert currentUser != null;
+                                final String currentUserId = currentUser.getUid();
+
+                                // create a userMap so that we can create a user in the User Collection in Firebase
+                                Map<String, String> userObj = new HashMap<>();
+                                userObj.put("userId", currentUserId);
+                                userObj.put("userName", userName);
+                                userObj.put("userEmail", userEmail);
+
+                                // adding users to Firestore
+                                collectionReference.add(userObj)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+                                                documentReference.get()
+                                                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                                if (Objects.requireNonNull(task.getResult()).exists()) {
+                                                                    String name = task.getResult().getString("userName");
+
+                                                                    Toast.makeText(SignupActivity.this, "SignUp Successful", Toast.LENGTH_LONG).show();
+                                                                    // if the user is registered successfully, redirect the user to DashBoardActivity
+                                                                    Intent i = new Intent(SignupActivity.this, DashBoardActivity.class);
+                                                                    i.putExtra("userName", name);
+                                                                    i.putExtra("userId", currentUserId);
+                                                                    startActivity(i);
+                                                                }
+                                                            }
+                                                        });
+                                            }
+                                        });
+
+
+                                // display successful msg and redirect user to login page
+                                //Toast.makeText(SignupActivity.this, "SignUp Successful", Toast.LENGTH_LONG).show();
+                                //startActivity(new Intent(SignupActivity.this, LoginActivity.class));
                             } else {
+                                // otherwise, display error msg
                                 Toast.makeText(SignupActivity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                             }
                         }

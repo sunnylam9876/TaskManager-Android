1a58938de40904b504cab259b423c979bdb557cb
Sunny Lam <77874599+sunnylam9876@users.noreply.github.com>
2023-11-19 18:50:36 -0800

    created home fragment

--- app/src/main/java/com/example/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/taskmanager/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.taskmanager;
 
+import static com.example.taskmanager.Utility.CalculateDate.monthFromDate;
+import static com.example.taskmanager.Utility.CalculateDate.yearFromDate;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.taskmanager.Calendar.MyCalendarAdapter;
+import com.example.taskmanager.Utility.FragmentUtility;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements MyCalendarAdapter.OnItemClickListener{
+
+    // for view binding
+    //ActivityMainBinding binding;
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);   // disabled since we use view binding
+        // for view binding
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        //replaceFragment(new HomeFragment());    // set default view
+        FragmentUtility.replaceFragment(MainActivity.this, new HomeFragment());
+
+        // assign Fragment to each bottom navigation button
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                //replaceFragment(new HomeFragment());  //if don't use FragmentUtility class
+                FragmentUtility.replaceFragment(MainActivity.this, new HomeFragment());
+            }
+            if (item.getItemId() == R.id.list) {
+                //replaceFragment(new ListFragment());
+                FragmentUtility.replaceFragment(MainActivity.this, new ListFragment());
+            }
+            if (item.getItemId() == R.id.AddItem) {
+                //replaceFragment(new ListFragment());
+                //FragmentUtility.replaceFragment(MainActivity.this, new ListFragment());
+
+            }
+            if (item.getItemId() == R.id.settings) {
+                //replaceFragment(new SettingFragment());
+                FragmentUtility.replaceFragment(MainActivity.this, new SettingFragment());
+            }
+            return true;
+        });
+
+        // for testing
+        Intent intent = getIntent();
+        if (intent != null) {
+            String userId = intent.getStringExtra("userId");
+            String userName = intent.getStringExtra("userName");
+            String userEmail = intent.getStringExtra("userEmail");
+            String userRole = intent.getStringExtra("userRole");
+        }
     }
+
+    @Override
+    public void onItemClick(String day) {
+        //Toast.makeText(this, "Selected Day: " + day + " " + monthFromDate(selectedDate) + " " + yearFromDate(selectedDate), Toast.LENGTH_SHORT).show();
+    }
+
+    // if don't use FragmentUtility class, un-comment the below statements
+    /*    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }*/
 }
---
+++ app/src/main/java/com/example/taskmanager/Utility/FragmentUtility.java
@@ -0,0 +1,18 @@
+package com.example.taskmanager.Utility;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.taskmanager.R;
+
+public class FragmentUtility extends FragmentActivity {
+    public static void replaceFragment(FragmentActivity activity, Fragment fragment) {
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
